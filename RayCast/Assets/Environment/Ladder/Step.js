e2aead0e028ec1a9a86d4383df90ea78e9fd393e
Oswell Chan <newageidiots@hotmail.com>
2015-03-15 16:58:32 +0800

    Ladder prefab

---
+++ RayCast/Assets/Environment/Ladder/Step.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+var playerObject : GameObject;
+ var canClimb = false;
+ var speed : float = 5;
+ 
+ function Start () {
+     playerObject = gameObject.Find("Player");
+ }
+ 
+ function OnTriggerEnter (coll : Collider){
+     if(coll.gameObject.name == "Player"){
+         canClimb = true;
+         var motor = playerObject.GetComponent(CharacterMotor);
+         motor.movement.gravity = 0;
+     }
+ }
+ 
+ function OnTriggerExit (coll2 : Collider){
+     if(coll2.gameObject.name == "Player"){
+         canClimb = false;
+         var motor = playerObject.GetComponent(CharacterMotor);
+         motor.movement.gravity = 20;
+     }
+ }
+ function Update () {
+     if(canClimb){
+     	var motor = playerObject.GetComponent(CharacterMotor);
+ 		motor.SetVelocity(Vector3(0,0,0) * Time.deltaTime*speed);
+         if(Input.GetKey(KeyCode.W)){
+         	motor = playerObject.GetComponent(CharacterMotor);
+    		motor.SetVelocity(Vector3(0,30,0) * Time.deltaTime*speed);
+         }
+         if(Input.GetKey(KeyCode.S)){
+         	motor = playerObject.GetComponent(CharacterMotor);
+            motor.SetVelocity(Vector3(0,-30,0) * Time.deltaTime*speed);
+         }
+     }
+ }
