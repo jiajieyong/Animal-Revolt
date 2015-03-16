9005f6291da7cff5eb0cc8324293c4978b0f5243
jackyhsuch <jackyhsu0102@gmail.com>
2015-03-16 15:48:54 +0800

    Enemy Movement (move towards player)

--- RayCast/Assets/Scripts/Enemy/EnemyMeleeMovement.js
+++ RayCast/Assets/Scripts/Enemy/EnemyMeleeMovement.js
@@ -1,33 +1,92 @@
 ﻿#pragma strict
 
 private var player : Transform;               // Reference to the player's position.
-//private var playerHealth : PlayerHealth;      // Reference to the player's health.
-//private var enemyHealth : EnemyHealth;        // Reference to this enemy's health.
+private var enemyHealth : EnemyHealth;        // Reference to this enemy's health.
 private var nav : NavMeshAgent;               // Reference to the nav mesh agent.
 
+var startingHealth : int = 100;           
+var currentHealth : int;    
+var timeSinceShot :float;
 
 function Awake ()
 {
     // Set up the references.
     player = GameObject.FindGameObjectWithTag ("Player").transform;
-    //playerHealth = player.GetComponent (PlayerHealth);
-    //enemyHealth = GetComponent (EnemyHealth);
+    enemyHealth = GetComponent (EnemyHealth);
     nav = GetComponent (NavMeshAgent);
+    currentHealth = startingHealth;
 }
 
 
 function Update ()
 {
-    // If the enemy and the player have health left...
-    //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-    //{
-        // ... set the destination of the nav mesh agent to the player.
-        nav.SetDestination (player.position);
-    //}
-    // Otherwise...
-    //else
-    //{
-        // ... disable the nav mesh agent.
-        //nav.enabled = false;
-    //}
+
+
+	if (timeSinceShot > 0) 
+	{
+	
+	
+		timeSinceShot += Time.deltaTime;
+		
+		if (timeSinceShot > 1) 
+		{
+
+			if (rigidbody.velocity.y == 0) 
+			{
+				nav.Resume();
+				Debug.Log("somethng");
+ 				rigidbody.isKinematic = true;
+ 				rigidbody.useGravity = false;
+ 				timeSinceShot = 0;
+ 			}
+		}
+	} 
+	else 
+	{
+	
+	    // If the enemy and the player have health left...
+	    if(currentHealth > 0)
+	    {
+	        nav.SetDestination (player.position);
+	    }
+	    else
+	    {
+	        nav.enabled = false;
+	    }
+    }
+}
+
+function ApplyDamage(damage: int){
+
+	
+	nav.Stop(true);
+ 	rigidbody.isKinematic = false;
+ 	rigidbody.useGravity = true;
+	timeSinceShot = Time.deltaTime;
+	
+	currentHealth -= damage;
+	
+    if(currentHealth <= 0)
+    {
+        Death ();
+    }
+
+}
+
+function Death () {
+    
+    // Find and disable the Nav Mesh Agent.
+    GetComponent (NavMeshAgent).enabled = false;
+
+    // Find the rigidbody component and make it kinematic (since we use Translate to sink the enemy).
+    GetComponent (Rigidbody).isKinematic = true;
+
+
+    // After 2 seconds destory the enemy.
+    Destroy (gameObject, 0f);
+
+}
+
+function OnGUI() {
+	GUI.Label(Rect(700,100,100,100), "health: " + currentHealth);
 }
