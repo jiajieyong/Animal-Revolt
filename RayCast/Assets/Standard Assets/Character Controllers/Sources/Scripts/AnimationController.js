883c84677a052111429ecd29a6a442cffe047f2a
kiangkuang <kiangkuang@gmail.com>
2015-03-27 21:48:36 +0800

    Jump animations

---
+++ RayCast/Assets/Standard Assets/Character Controllers/Sources/Scripts/AnimationController.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+var animator : Animator;
+var characterMotor : CharacterMotor;
+
+function Start () {
+	animator = GetComponentInChildren(Animator);
+	characterMotor = GetComponent(CharacterMotor);
+}
+
+function Update () {
+	animator.SetBool ("Grounded", characterMotor.grounded);
+	animator.SetFloat ("Forward", Input.GetAxis ("Vertical"));
+	animator.SetFloat ("Side", Input.GetAxis ("Horizontal"));
+}
