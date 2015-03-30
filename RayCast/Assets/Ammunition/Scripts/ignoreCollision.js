51a28adfaee9111847d1533f78cd5b7b5d3514a1
jackyhsuch <jackyhsu0102@gmail.com>
2015-03-30 11:14:46 +0800

    Rat bullet - fan shoot

---
+++ RayCast/Assets/Ammunition/Scripts/ignoreCollision.js
@@ -0,0 +1,9 @@
+﻿ #pragma strict
+ 
+ var radius = 0.051;
+ 
+     function Update () {
+       if (!collider.enabled && !Physics.CheckSphere(transform.position, radius)) {
+         collider.enabled = true;
+         }
+     }
