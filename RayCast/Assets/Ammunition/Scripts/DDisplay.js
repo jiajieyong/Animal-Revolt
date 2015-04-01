3cfeb8bde7d96afead318f0d98c9809cb64138f4
Oswell Chan <newageidiots@hotmail.com>
2015-04-01 14:45:52 +0800

    Damage Display

---
+++ RayCast/Assets/Ammunition/Scripts/DDisplay.js
@@ -0,0 +1,75 @@
+﻿#pragma strict
+var tpsCamera: Camera;
+var fpsCamera: Camera;
+var style : GUIStyle;
+
+private var tpsON = false; 
+private var msgList = new List.<Container>();
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetMouseButtonDown(1)){
+		tpsON = !tpsON;
+	}
+}
+
+function DisplayDamage(c : Container) {
+	c._time = 0;
+	msgList.Add(c);
+}
+
+class Container {
+	var _dmg : int;
+	var _transform : Transform;
+	var _type : String;
+	var _time : float;
+	
+	function Container(dmg : int, pos : Transform, type : String) {
+		_dmg = dmg;
+		_transform = pos;
+		_type = type;
+	}
+}
+
+function OnGUI() {
+	var count : int = msgList.Count;
+	var time = Time.deltaTime;
+	
+	for (var i = 0; i < count; i++) {
+		var msg = msgList[0];
+		
+		if (msg._time > 2) {
+			msgList.RemoveAt(0);
+		}
+		
+		if (msg._type == "instant") {
+			style.normal.textColor = Color.yellow;
+		}
+		
+		if (msg._transform != null) {
+			var pos = msg._transform.position;
+
+		
+		var currentCam = chooseCamera();
+		var heading = pos - currentCam.transform.position;
+		if (Vector3.Dot(currentCam.transform.forward, heading) > 0) {
+    		var screenPos : Vector3 = currentCam.WorldToScreenPoint(pos);
+			GUI.Label(Rect(screenPos.x, Screen.height - screenPos.y - 50,60, 60), "" + msg._dmg, style);;
+		}
+		}
+		msg._time += time;
+	}
+	
+	
+}
+
+function chooseCamera() { 
+	if (tpsON) {
+		return tpsCamera;
+	} else {
+		return fpsCamera;
+	}
+}
--- RayCast/Assets/Ammunition/Scripts/stoneBullet.js
+++ RayCast/Assets/Ammunition/Scripts/stoneBullet.js
@@ -2,19 +2,25 @@
  
 var TheDamage = 100;
 var direction : Vector3;
+var damageDisplay : GameObject;
 
 function Start () {
 	rigidbody.AddTorque(Vector3(10.0, 0, 10.0));
+	damageDisplay = GameObject.FindGameObjectsWithTag ("Display")[0];
 }
  
  function OnCollisionEnter (info : Collision)
  {	
  	if (info.gameObject.tag == "Player" && this.gameObject.tag == "EnemyBullet") {
+ 		//damageDisplay.transform.SendMessage("DisplayDamage", TheDamage, "instant");
     	info.transform.SendMessage("ApplyDamage", TheDamage, SendMessageOptions.DontRequireReceiver);
     	Destroy(this.gameObject);
     }
    else if (info.gameObject.tag == "Enemy" && this.gameObject.tag == "PlayerBullet") {
+   		var container = new Container(TheDamage, info.collider.transform, "instant");
+   		damageDisplay.transform.SendMessage("DisplayDamage", container);
     	info.transform.SendMessage("ApplyDamage", TheDamage, SendMessageOptions.DontRequireReceiver);
     	Destroy(this.gameObject);
     }
  }
+ 
