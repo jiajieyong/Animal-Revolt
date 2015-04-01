bb340334d7bd1f419f9070f331826d2571c9e8c1
kiangkuang <kiangkuang@gmail.com>
2015-04-01 23:42:14 +0800

    Cleaned cleanest cleaner master Jacky sama

--- RayCast/Assets/Camera/crosshair.js
+++ RayCast/Assets/Camera/crosshair.js
@@ -1,13 +1,14 @@
 ﻿#pragma strict
 
 var crosshair : Texture2D;
+var enemyManager : EnemyManager;
  
 function Start () {
         Screen.showCursor = false;
 }
  
 function OnGUI () { 
-		if (!changeAmmo.cowMode) {
+		if (!changeAmmo.cowMode && !enemyManager.startOfWave) {
         GUI.DrawTexture(Rect(Screen.width * 0.5 - 32,Screen.height * 0.5 - 32,64,64), crosshair);
         }
 }
--- RayCast/Assets/Enemies Spwan/EnemyManager.js
+++ RayCast/Assets/Enemies Spwan/EnemyManager.js
@@ -1,15 +1,20 @@
-#pragma strict
+	#pragma strict
 
 var playerHealth : playerHealth;       // Reference to the player's heatlh.
 var enemy : GameObject;                // The enemy prefab to be spawned.
-var spawnTime : float = 3f;            // How long between each spawn.
+var spawnTime : float = 5f;            // How long between each spawn.
 var spawnPoints : Transform[];         // An array of the spawn points this enemy can spawn from.
+var style : GUIStyle; 
+
 
 var spawnMode : SpawnType;
 var totalWaves : float = 3f;
-var numWaves : float = 0f;
-public static var enemyDeathCount : float = 0f;
+private var numWaves : float = 1f;
+private var enemyDeathCount : float = 0f;
 var spawn = true;
+private var totalEnemies : float = 0f;
+private var showLabel = false;
+var startOfWave = true;
 
 
 function Start ()
@@ -25,27 +30,71 @@ function StartSpawn() {
 
 		
 	while (spawn) {
+		Debug.Log("spawn continues");
 		switch(spawnMode) {
 			case SpawnType.Normal: 
-				InvokeRepeating ("Spawn", spawnTime, spawnTime);
+				yield WaitForSeconds(spawnTime);
+				Spawn();
+				
+				if (playerHealth.playerHealth <= 0f) 
+					spawn = false;
+					
+				break;
 			
 			case SpawnType.Wave:
 				if (numWaves <= totalWaves) {
-					yield WaitForSeconds(spawnTime);
-					Spawn();
+				
+					if(startOfWave) {
+						yield WaitForSeconds(1);
+						ToggleLable();
+						yield WaitForSeconds(3);
+						ToggleLable();
+						
+						startOfWave = false;
+					}
+					
+				
+					Debug.Log(enemyDeathCount);
+					
+					if (totalEnemies < numWaves*5f && enemyDeathCount <= numWaves*5f) {
+						yield WaitForSeconds(spawnTime);
+						Spawn();
+					}
+					
+					if (enemyDeathCount >= numWaves*5f) {
+						Debug.Log("here");
+						enemyDeathCount = 0f;
+						totalEnemies = 0f;
+						numWaves++;
+						yield WaitForSeconds(5);
+						startOfWave = true;
+						break;
+					}
+					
+					break;
+					
+					
+					
+					
+					
+				} else {
+					spawn = false;
 				}
+				
+				break;
 			
 			
 			default:
             	spawn = false;
                 break;
 		}
+		
+		yield new WaitForEndOfFrame ();
 			
 	}
 	
 
 }
-
 
 function Spawn ()
 {
@@ -59,5 +108,28 @@ function Spawn ()
     // Find a random index between zero and one less than the number of spawn points.
     var spawnPointIndex : int = Random.Range (0, spawnPoints.Length);
     // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-    Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    var clone = Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    
+    clone.GetComponent(Enemy).updateEM(gameObject);
+    
+    totalEnemies++;
+}
+
+function EnemyDeath() {
+	
+	if (spawnMode==SpawnType.Wave)
+		enemyDeathCount++;
+
+}
+
+function ToggleLable() {
+	showLabel = !showLabel;
+}
+
+function OnGUI () { 
+
+	if (showLabel) {
+		GUI.Label(Rect(Screen.width/2 - 50,Screen.height/2 - 25 ,100, 50), "WAVE " + numWaves, style); 
+	}
+
 }
