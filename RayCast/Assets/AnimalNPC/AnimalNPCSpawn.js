72b136191c181edd74459468f94b1f46c2e0d844
jackyhsuch <jackyhsu0102@gmail.com>
2015-04-03 18:29:44 +0800

    Max enemy in Normal Mode

---
+++ RayCast/Assets/AnimalNPC/AnimalNPCSpawn.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function Spawn ()
+{
+
+
+//    var clone = Instantiate (enemy, transform.position, transform.rotation);
+ //   clone.GetComponent(Enemy).updateEnemy(gameObject, player);
+  
+}
