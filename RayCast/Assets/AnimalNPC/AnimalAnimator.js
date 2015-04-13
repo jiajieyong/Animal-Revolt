704b9dea0eacb58320db2070a52f4683bce2cf45
kiangkuang <kiangkuang@gmail.com>
2015-04-13 17:52:15 +0800

    Animal animation

--- RayCast/Assets/AnimalNPC/AnimalAnimator.js
+++ RayCast/Assets/AnimalNPC/AnimalAnimator.js
@@ -12,7 +12,7 @@ function Start () {
 function Update () {
 	var pos = Vector2(rigidbody.transform.position.x, rigidbody.transform.position.z) ;
 
- 	var movement = (pos - originalPos) * 10;
+ 	var movement = (pos - originalPos) * 100;
  	var magnitude = movement.magnitude;
  	
  	if (originalMag >= magnitude){
@@ -25,11 +25,4 @@ function Update () {
  	
 	animator.SetFloat ("Forward", originalMag);
 	originalPos = pos;
-	
-	if (Physics.Raycast(transform.position, -Vector3.up, 0.3)) {
-		animator.SetBool ("Grounded", true);
-	} else {
-		animator.SetBool ("Grounded", false);
-	}
-	
 }
