d3f449388a70abf1b044e6295556c8551db7f76f
kiangkuang <kiangkuang@gmail.com>
2015-04-13 21:01:04 +0800

    Anti pushing collider

---
+++ RayCast/Assets/antiCollide.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+private var motor : CharacterMotor;
+
+function Start() {
+	motor = GameObject.Find("/First Person Controller").GetComponent(CharacterMotor);
+}
+
+function OnCollisionEnter(collision : Collision) {
+	motor.movement.velocity = Vector3(0,0,0);
+}
