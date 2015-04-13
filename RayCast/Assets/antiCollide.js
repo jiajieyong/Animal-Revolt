bacc51dee1f50e0e06840d54bb259261938102ba
kiangkuang <kiangkuang@gmail.com>
2015-04-13 22:54:11 +0800

    frog animation

--- RayCast/Assets/antiCollide.js
+++ RayCast/Assets/antiCollide.js
@@ -6,6 +6,6 @@ function Start() {
 	motor = GameObject.Find("/First Person Controller").GetComponent(CharacterMotor);
 }
 
-function OnCollisionEnter(collision : Collision) {
+function OnTriggerEnter (other : Collider) {
 	motor.movement.velocity = Vector3(0,0,0);
 }
