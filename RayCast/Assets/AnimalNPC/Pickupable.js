4735f38ce7a4fb6dfbfb52e1912156b0f7da6c22
kiangkuang <kiangkuang@gmail.com>
2015-04-17 17:32:52 +0800

    new navmesh and etc

---
+++ RayCast/Assets/AnimalNPC/Pickupable.js
@@ -0,0 +1,26 @@
+﻿#pragma implicit
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(other: Collider) {
+		if (other.transform.CompareTag("inventory")) {
+			var ob : Behaviour = GetComponent("Halo");
+			ob.enabled = true;
+		} 
+		
+}
+
+function OnTriggerExit(other: Collider) {
+		if (other.transform.CompareTag("inventory")) {
+			var ob : Behaviour = GetComponent("Halo");
+			ob.enabled = false;
+		} 
+		
+		
+}
