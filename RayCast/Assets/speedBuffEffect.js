424eaccfa9cfed82bcaf205ed640bd606bcacd7d
kiangkuang <kiangkuang@gmail.com>
2015-04-22 00:24:21 +0800

    horse speed buff

---
+++ RayCast/Assets/speedBuffEffect.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+var controller: GameObject;
+var buff = false;
+
+var speed : Texture2D;
+function Start () {
+
+}
+
+function Update () {
+	if (buff == true) {
+		controller.GetComponent(CharacterMotor).movement.maxForwardSpeed = 50;
+		controller.GetComponent(CharacterMotor).movement.maxSidewaysSpeed = 50;
+		controller.GetComponent(CharacterMotor).movement.maxBackwardsSpeed = 50;
+		controller.GetComponent(CharacterMotor).movement.maxGroundAcceleration = 200;
+	} else {
+		controller.GetComponent(CharacterMotor).movement.maxForwardSpeed = 5;
+		controller.GetComponent(CharacterMotor).movement.maxSidewaysSpeed = 5;
+		controller.GetComponent(CharacterMotor).movement.maxBackwardsSpeed = 5;
+		controller.GetComponent(CharacterMotor).movement.maxGroundAcceleration = 20;
+	}
+}
+
+function hax() {
+	if (buff == false) {
+		buff = true;
+		yield WaitForSeconds (10);
+		buff = false;
+	}
+}
+
+function OnGUI () {
+	if (buff == true) {
+        GUI.DrawTexture(Rect(Screen.width * 0.5 - 380,Screen.height - 90 ,64,64), speed);
+	}
+}
---
+++ RayCast/Assets/speedbuff.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+var speedBuff: GameObject;
+var speedCooldown = 11;
+var inventory : GameObject;
+private var timer = 0.0;
+
+function Start () {
+	speedBuff = GameObject.Find("First Person Controller");
+}
+
+function Update () {
+	if (Input.GetMouseButtonDown(0) && GameObject.Find("Canvas").GetComponent(LoadOnClickPause).pauseGame == false && timer <= Time.time) {
+		speedBuff.transform.BroadcastMessage ("hax");	
+		timer = Time.time + speedCooldown;
+		inventory.GetComponent(Inventory).decrementBullet();
+	}
+}
